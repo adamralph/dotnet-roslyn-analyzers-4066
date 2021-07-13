7f74206e723ffcff07a5693bf3e3969726d5cfbd
Adam Ralph <adam@adamralph.com>
2021-07-13 19:51:42 +0200

    add Foo.cs for CA1801 true positives

---
+++ Foo.cs
@@ -0,0 +1,15 @@
+﻿namespace Test
+{
+    public static class Foo
+    {
+        public static void Bar(int unused)
+        {
+            Baz(123);
+        }
+
+        private static void Baz(int unused)
+        {
+            System.Console.WriteLine();
+        }
+    }
+}
